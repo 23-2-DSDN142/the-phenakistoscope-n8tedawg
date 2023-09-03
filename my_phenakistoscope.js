52108dabc12a1f97da4bc08047d06f02b92a4e42
n8tedawg <135954853+n8tedawg@users.noreply.github.com>
2023-09-04 08:42:31 +1200

    added notes
    sadly worked on a different file rather than this file

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -1,29 +1,34 @@
-const SLICE_COUNT = 10;
+const SLICE_COUNT = 10;//(Min 6, Max 18, recommended 10) 
+// This changes how many slices the phenakistoscope has. More slices means it will move slower.
 
 function setup_pScope(pScope){
-  pScope.output_mode(ANIMATED_DISK);
+  pScope.output_mode(ANIMATED_DISK);// change the mode using the setting
   pScope.scale_for_screen(true);
-  pScope.draw_layer_boundaries(true);
-  pScope.set_direction(CCW);
+  pScope.draw_layer_boundaries(true); //remove boundary layers, true or false 
+  pScope.set_direction(CCW); //  Counter Clock Wise or Clock Wise. Affects how it spins.
   pScope.set_slice_count(SLICE_COUNT);
 }
 
 function setup_layers(pScope){
 
-  new PLayer(null, 220);  //lets us draw the whole circle background, ignoring the boundaries
+  new PLayer(null, 50, 168, 168);  //lets us draw the whole circle background, ignoring the boundaries. the second number value determines colour.
 
-  var layer1 = new PLayer(faces);
-  layer1.mode( SWIRL(5) );
-  layer1.set_boundary( 200, 1000 );
+  var layer1 = new PLayer(faces);// insert to faces the function you are using
+  layer1.mode( SWIRL(5) ); // number determines how many of the object show on each slice. SWIRL goes out from the middle, towards the end
+  layer1.set_boundary( 200, 1000 ); // the first number talks about the length from the origin and where the object starts.
+  // second number talks about how far it goes from the origin to the edge of the radius.
 
   var layer2 = new PLayer(squares);
-  layer2.mode( RING );
+  layer2.mode( RING ); // i think the ring moves in a ring-like shape. idk
   layer2.set_boundary( 0, 400 );
 }
 
-function faces(x, y, animation, pScope){
+function faces(x, y, animation, pScope){ // emitting this affects the animation and makes it a swirl instead for some odd reason.
   
-  scale(animation.frame*2);
+  scale(animation.frame*2);// emitting this affects the animation and makes it a swirl instead for some odd reason.
+  // scale(animation.wave*2) // oddly makes the face repeat multiple times
+  // making this animation.frame says to the code "from the origin, start at 0 and when you reach the end, make them a size of 1"
+  // you can add scale(n), n = the size of all the faces.
 
   ellipse(0,0,50,50); // draw head
   fill(30);
