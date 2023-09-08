ca094209d55761b3805919df58f215ec91c2a0ec
n8tedawg <135954853+n8tedawg@users.noreply.github.com>
2023-09-08 16:27:01 +1200

    edited some things

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -2,7 +2,8 @@ const SLICE_COUNT = 10;//(Min 6, Max 18, recommended 10)
 // This changes how many slices the phenakistoscope has. More slices means it will move slower.
 
 function setup_pScope(pScope){
-  pScope.output_mode(ANIMATED_DISK);// change the mode using the setting
+  //pScope.output_mode(STATIC_DISK);// change the mode using the setting
+  pScope.output_mode(ANIMATED_DISK);
   pScope.scale_for_screen(true);
   pScope.draw_layer_boundaries(true); //remove boundary layers, true or false 
   pScope.set_direction(CCW); //  Counter Clock Wise or Clock Wise. Affects how it spins.
@@ -11,7 +12,7 @@ function setup_pScope(pScope){
 
 function setup_layers(pScope){
 
-  new PLayer(null, 50, 168, 168);  //lets us draw the whole circle background, ignoring the boundaries. the second number value determines colour.
+  new PLayer(null, 242, 221, 150);  //lets us draw the whole circle background, ignoring the boundaries. the second number value determines colour.
 
   let layer1 = new PLayer(faces);// insert to faces the function you are using
   layer1.mode( SWIRL(4) ); // number determines how many of the object show on each slice. SWIRL goes out from the middle, towards the end
@@ -27,7 +28,7 @@ function setup_layers(pScope){
 function faces(x, y, animation, pScope){ // emitting this affects the animation and makes it a swirl instead for some odd reason.
   
   scale(animation.frame*3);// emitting this affects the animation and makes it a swirl instead for some odd reason.
-  // scale(animation.wave*2) // oddly makes the face repeat multiple times
+  //scale(animation.wave*2) // oddly makes the face repeat multiple times
   // making this animation.frame says to the code "from the origin, start at 0 and when you reach the end, make them a size of 1"
   // you can add scale(n), n = the size of all the faces.
 
@@ -37,7 +38,6 @@ function faces(x, y, animation, pScope){ // emitting this affects the animation 
   // ellipse(-10,-10,10,10); //draw eye
   // ellipse(10,-10,10,10); // draw eye
   // arc(0,10,20,10,0,180); // draw mouth
-
 }
 
 function squares(x, y, animation, pScope){
@@ -47,10 +47,17 @@ function squares(x, y, animation, pScope){
   let backgroundArcStart = 270 - angleOffset;
   let backgroundArcEnd = 270 + angleOffset;
 
-  fill(200, 135, 245)
-  arc(x,y,800,800,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
+  // draws the inside circle
+  fill(200, 135, 245) // colour of innner circle
+  arc(x,y,400,400,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
+  // changing the number changes the size of the inner circle.
 
+  // draws the small white squares in the inside circle
   fill(255)
-  rect(-10,-300-animation.wave()*50,20,20) // .wave is a cosine wave btw
-
+  rect(-30,-300-animation.wave()*50,70,70) // .wave is a cosine wave btw
+  //ellipse(-10,-300-animation.wave()*50,70,70)
+  // number multiplied to animation.wave determines how fast and far it moves
+  // second to last number is how far it stretches out horizontally
+  // last number is how far it stretches out vertically
 }
+
