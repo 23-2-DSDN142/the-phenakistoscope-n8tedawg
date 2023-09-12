19560a0b50898084a139d8189b1e53226ec59d56
n8tedawg <135954853+n8tedawg@users.noreply.github.com>
2023-09-12 22:33:45 +1200

    added new lava and new flower lei

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -11,6 +11,7 @@ function setup_pScope(pScope){
   //pScope.load_image("tiki_test_0" , "png"); //big design
   //pScope.load_image_sequence("tiki_test", "png", 1)
   pScope.load_image_sequence("tiki_explode", "png", 12);
+  pScope.load_image("flower_lei" , "png");
   //pScope.load_image("tiki_explode_0" , "png"); //smaller design
 }
 
@@ -18,11 +19,15 @@ function setup_layers(pScope){
 
   new PLayer(null, 242, 221, 150);  //lets us draw the whole circle background, ignoring the boundaries. the second number value determines colour.
 
-  let layer1 = new PLayer(mushroom_clouds);// insert to faces the function you are using
-  layer1.mode( SWIRL(1) ); // number determines how many of the object show on each slice. SWIRL goes out from the middle, towards the end
+//  let boom = new PLayer(mushroom_clouds);// insert to faces the function you are using
+//  boom.mode( SWIRL(1) ); // number determines how many of the object show on each slice. SWIRL goes out from the middle, towards the end
   //number technically means how many swirls it needs to complete
-  layer1.set_boundary( 600, 1100 ); // the first number talks about the length from the origin and where the object starts.
+//  boom.set_boundary( 600, 1100 ); // the first number talks about the length from the origin and where the object starts.
   // second number talks about how far it goes from the origin to the edge of the radius.
+
+  let garland = new PLayer(flowers);
+  garland.mode( SWIRL(1) );
+  garland.set_boundary( 0, 300 );
 
   //let layer2 = new PLayer(squares);
   //layer2.mode( RING ); // i think the ring moves in a ring-like shape. idk
@@ -32,23 +37,32 @@ function setup_layers(pScope){
   tikisequence.mode( RING );
   tikisequence.set_boundary( 0, 200 );
   //first number determines the length from the origin. The second number talks about how far it goes from origin to edge of circle
+
+  let eruption = new PLayer(molten);
+  eruption.mode( SWIRL(3) );
+  eruption.set_boundary( 600, 1400 );
 }
 
 // THE TIKI EXPLODING
 function tiki(x, y, animation, pScope){
 
+  // let tikiSize = 100 + (animation.wave(1)*20)
+  // let bouncepart = 50* animation.wave()
+
   //scale(0.1); //tiki_test_0
-  scale(0.65); //tiki_explode_0 // literally makes the scale bigger
+  scale(0.6); //tiki_explode_0 // literally makes the scale bigger
   pScope.draw_image_from_sequence("tiki_explode", 0, -900, animation.frame);
  //pScope.draw_image_from_sequence("tiki_test", 0, -150, animation.frame);
  //pScope.draw_image("tiki_test_0",x,-5000); // big design
  // pScope.draw_image("tiki_explode_0",x,-700); //smaller design
  
  //the number on the edge determines how far apart they are from each other
+
 }
 
-
-function mushroom_clouds(x, y, animation, pScope){ // emitting this affects the animation and makes it a swirl instead for some odd reason.
+//first number moves it left if negative and right if positive
+//second number moves it up negative and down positive
+ function mushroom_clouds(x, y, animation, pScope){ // emitting this affects the animation and makes it a swirl instead for some odd reason.
   
   scale(animation.frame*3);// emitting this affects the animation and makes it a swirl instead for some odd reason.
   //scale(animation.wave*2) // oddly makes the face repeat multiple times
@@ -63,8 +77,7 @@ function mushroom_clouds(x, y, animation, pScope){ // emitting this affects the 
   strokeWeight(0)
   fill(252, 136, 3)
   ellipse (-35,-20,60,60); //left cloud
-  //first number moves it left if negative and right if positive
-  //second number moves it up negative and down positive
+  
   ellipse (35,-20,60,60); //right cloud
   ellipse (0,-50,60,60); //middle cloud
   
@@ -79,9 +92,22 @@ function mushroom_clouds(x, y, animation, pScope){ // emitting this affects the 
   // vertex(x,y);
   // bezierVertex();
   // endShape();
+}
 
-  
+function flowers(x, y, animation, pScope){
+  push()
+  scale(1.4)
+  if(animation.frame ==0){
+    pScope.draw_image("flower_lei",x,y);
+  }
+  pop()
 
+  push()
+  scale(0.65)
+  if(animation.frame ==0){
+    pScope.draw_image("flower_lei",x,y);
+  }
+  pop()
 }
 
 function squares(x, y, animation, pScope){
@@ -105,3 +131,28 @@ function squares(x, y, animation, pScope){
   // last number is how far it stretches out vertically
 }
 
+function molten(x, y, animation, pScope){
+
+  let moltenX = 0
+  let moltenY = -26
+
+  scale (animation.frame*3);
+
+  strokeWeight(0)
+  fill(252, 136, 3)
+  ellipse(0,0,60,100);
+  //quad(moltenX, moltenY, moltenX-45, moltenY+50, moltenX, moltenY+100, moltenX+45, moltenY+50);
+  triangle(moltenX, moltenY+100, moltenX-27, moltenY+50, moltenX+27, moltenY+50);
+
+  let moltenXL = -90
+  let moltenYL = 20
+  strokeWeight(0)
+  ellipse(moltenXL, moltenYL+26 ,60 ,100);
+  triangle(moltenXL, moltenYL+100, moltenXL-27, moltenYL+50, moltenXL+27, moltenYL+50);
+
+  let moltenXR = 90
+  let moltenYR = 20
+  strokeWeight(0)
+  ellipse(moltenXR, moltenYR+26 ,60 ,100);
+  triangle(moltenXR, moltenYR+100, moltenXR-27, moltenYR+50, moltenXR+27, moltenYR+50);
+}
